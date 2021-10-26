6c6c55e7bdc005c9e1aa15d3c9a8e2333f75f456
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-26 15:30:46 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function EntryForm({ addEntryToPhoneBook }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addEntryToPhoneBook({ firstName, lastName, phoneNumber });
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        name="lastName"
+        id="lastName"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="text"
+        name="phoneNumber"
+        id="phoneNumber"
+        placeholder="Phone Number"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
+      <button type="submit" onSubmit={handleSubmit}>
+        submit
+      </button>
+    </form>
+  );
+}
+
+function DisplayEntries({ entries }) {
+  return (
+    <table style={{ marginTop: "1em", width: 300 }}>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Phone Number</th>
+        </tr>
+      </thead>
+      <tbody style={{ marginTop: ".5em" }}>
+        {entries.map((entry) => (
+          <tr key={`${entry.firstName} ${entry.lastName}`}>
+            <td>{entry.firstName}</td>
+            <td>{entry.lastName}</td>
+            <td>{entry.phoneNumber}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 function App() {
+  const [entries, setEntries] = useState([]);
+
+  const addEntryToPhoneBook = (entry) => {
+    setEntries(
+      [...entries, entry].sort((a, b) =>
+        a.lastName.toLowerCase() > b.lastName.toLowerCase() ? 1 : -1
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <EntryForm addEntryToPhoneBook={addEntryToPhoneBook} />
+      <DisplayEntries entries={entries} />
     </div>
   );
 }
